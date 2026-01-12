37bda6269e130f8022459c3df8b12e44cc5251d6
Matt Linn <mattlinn@Matts-MacBook-Air.local>
2026-01-12 21:53:18 +1030

    Fix iOS Safari viewport shrink

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import PasswordGate from './PasswordGate';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PasswordGate>
-      <App />
-    </PasswordGate>
+    <App />
   </React.StrictMode>
 );
 
