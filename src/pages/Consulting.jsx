1790f91e29fe7227dcba29c05b083a1715ecf189
Matt Linn <mattlinn@Matts-MacBook-Air.local>
2026-01-23 14:07:58 +1030

    Remove unused QuoteIcon component

--- src/pages/Consulting.jsx
+++ src/pages/Consulting.jsx
@@ -708,14 +708,6 @@ function CheckIcon({ color, size = 14 }) {
   );
 }
 
-function QuoteIcon({ color }) {
-  return (
-    <svg width="32" height="32" viewBox="0 0 24 24" fill={color}>
-      <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
-    </svg>
-  );
-}
-
 function GraduateIcon({ color }) {
   return (
     <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
